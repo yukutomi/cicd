bb1bd37b964d0ef2fa8b3b7c4098190a25f56c8c
yukutomi <yukutomi@m.ieice.org>
2020-07-27 14:17:24 +0900

    test2

---
+++ pipeline_settings/lambda_handler.py
@@ -0,0 +1,11 @@
+import json                                                                                                                                                                                                 
+import numpy as np
+
+
+def lambda_handler(event, context):
+    arr = np.asarray([1,2,3])
+    return {
+        'statusCode': 200,
+        'body': json.dumps(str(arr))
+    }
+
