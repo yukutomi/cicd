b7d7f6ed3564ed509b51b913cd0bee63aca9fa24
yukutomi <yukutomi@m.ieice.org>
2020-07-27 13:58:41 +0900

    test

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,6 +1,7 @@
 import json                                                                                                                                                                                                 
 import numpy as np
 
+
 def lambda_handler(event, context):
     arr = np.asarray([1,2,3])
     return {
